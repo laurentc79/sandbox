5e8b7fb57e50ba8586fc5208d14dcbb4a03c52cc
CHAVIGNON Laurent <L.CHAVIGNON@alptis.fr>
2024-04-09 11:25:48 +0200

    first commit

---
+++ src/main/java/org/laurentc79/sandboxspringboot/SandboxSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.laurentc79.sandboxspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/laurentc79/sandboxspringboot/SandboxSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.laurentc79.sandboxspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
